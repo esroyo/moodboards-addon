074c8b29835f8efc3f8b90568925893714b09b03
Carles Escrig Royo <esroyo@gmail.com>
2020-02-23 23:47:52 +0100

    Add signed .xpi

---
+++ web-ext-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    verbose: false,
+    ignoreFiles: [
+        "*.git*",
+        "bower.json",
+        "web-ext-config.js",
+        ".bowerrc",
+        "web-ext-artifacts/",
+        "vendor/!(fabric|foundation)**/"
+        // "vendor/!(foundation/css/normalize.css|foundation/css/foundation.css|foundation/js/vendor/jquery.js|foundation/js/foundation.min.js|fabric/dist/fabric.min.js)"
+
+    ]
+};
